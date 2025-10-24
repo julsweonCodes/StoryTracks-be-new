169e192fd927bde84e1904eaab0ab9f9d2db3905
julsweonCodes <julsweon@gmail.com>
2025-10-24 12:22:34 +0900

    first commit

---
+++ src/main/java/com/T4/storyTracks/StoryTracksApplication.java
@@ -0,0 +1,13 @@
+package com.T4.storyTracks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryTracksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryTracksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/T4/storyTracks/StoryTracksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.T4.storyTracks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryTracksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
